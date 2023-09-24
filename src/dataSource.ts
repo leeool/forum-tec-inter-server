f1999d14790b96dea06be9e4f6a814e192bee8fa
leeool <lgonsalez02@gmail.com>
2023-09-24 03:04:24 -0300

    feat: update ssl

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -9,6 +9,9 @@ const appDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  },
   entities: [__dirname + "/**/entities/*.{js,ts}"],
   migrations: [__dirname + "/**/migrations/*.{js,ts}"]
 })
